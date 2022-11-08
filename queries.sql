629220a251bd4cadd33f954900f85728d7491fbc
samishsK <samish.sk@gmail.com>
2022-11-07 22:02:35 -0800

    Data created for Reminders table

--- queries.sql
+++ queries.sql
@@ -8,20 +8,19 @@ CREATE TABLE IF NOT EXISTS Events (
 );
 
 CREATE TABLE IF NOT EXISTS Reminders (
+    r_userid INTEGER NOT NULL,
     r_title VARCHAR(255) NOT NULL,
     r_description VARCHAR(255) NOT NULL,
     r_recurring BOOLEAN NOT NULL,
     r_priority VARCHAR(255) NOT NULL,
-    r_date datetime NOT NULL
+    r_date TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Status (
     s_title VARCHAR(255) NOT NULL,
-    s_draft VARCHAR(255) NOT NULL,
+    s_draft BOOLEAN NOT NULL,
     s_open BOOLEAN NOT NULL,
-    s_closed BOOLEAN NOT NULL,
     s_ongoing BOOLEAN NOT NULL,
-    s_past BOOLEAN NOT NULL,
     s_cancelled BOOLEAN NOT NULL
 );
 
@@ -39,7 +38,7 @@ CREATE TABLE IF NOT EXISTS Meetings (
 ); 
 
 CREATE TABLE IF NOT EXISTS Users (
-    u_userid INTEGER NOT NULL,
+    u_id INTEGER NOT NULL,
     u_firstname VARCHAR(255) NOT NULL,
     u_lastname VARCHAR(255) NOT NULL,
     u_email VARCHAR(255) NOT NULL,
@@ -56,3 +55,9 @@ INSERT INTO Events(e_title, e_description, e_location, e_attendee, e_groupid, e_
 INSERT INTO Events(e_title, e_description, e_location, e_attendee, e_groupid, e_userid) VALUES ('Run', 'daily run', 'Downtown', ' ', 4, 2);
 INSERT INTO Events(e_title, e_description, e_location, e_attendee, e_groupid, e_userid) VALUES ('Dinner', 'dinner with friend', 'Cheesecake Factory', 'Jack', 5, 4);
 INSERT INTO Events(e_title, e_description, e_location, e_attendee, e_groupid, e_userid) VALUES ('Lunch', 'lunch with coworker', 'Poke', 'Travis', 5, 4);
+
+INSERT INTO Reminders(r_userid, r_title, r_description,  r_recurring,  r_priority, r_date) VALUES (1, 'Midterm 1', 'MATH-141', FALSE, 'High', '2022-11-17 13:00');
+INSERT INTO Reminders(r_userid, r_title, r_description,  r_recurring,  r_priority, r_date) VALUES (1, 'Final Test', 'MATH-141', FALSE, 'High', '2022-12-17 15:00');
+INSERT INTO Reminders(r_userid, r_title, r_description,  r_recurring,  r_priority, r_date) VALUES (3, 'Code Review', 'Team code review', TRUE, 'Medium', '2022-11-15 10:00');
+INSERT INTO Reminders(r_userid, r_title, r_description,  r_recurring,  r_priority, r_date) VALUES (5, 'Product Launch', 'Watch Party', FALSE, 'High', '2022-11-26 09:00');
+INSERT INTO Reminders(r_userid, r_title, r_description,  r_recurring,  r_priority, r_date) VALUES (4, 'Meeting', 'Team meeting', TRUE, 'High', '2022-12-05 11:00');
