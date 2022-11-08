2e5c3af6b82f23e9b00e4d6436ab2481ec6daa5e
samishsK <samish.sk@gmail.com>
2022-11-07 20:07:47 -0800

    Created 4 tables

---
+++ queries.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS Events (
+    e_location VARCHAR(255) NOT NULL,
+    e_attendees VARCHAR(255) NOT NULL,
+    e_creator VARCHAR(255) NOT NULL,
+    e_title VARCHAR(255) NOT NULL,
+    e_description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Reminders (
+    r_recurring BOOLEAN NOT NULL,
+    r_priority VARCHAR(255) NOT NULL,
+    r_title VARCHAR(255) NOT NULL,
+    e_description VARCHAR(255) NOT NULL,
+    e_date datetime NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Status (
+    s_draft VARCHAR(255) NOT NULL,
+    s_open BOOLEAN NOT NULL,
+    s_closed BOOLEAN NOT NULL,
+    s_ongoing BOOLEAN NOT NULL,
+    s_past BOOLEAN NOT NULL,
+    s_cancelled BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Groups (
+    g_title VARCHAR(255) NOT NULL,
+    g_type VARCHAR(255) NOT NULL
+);
